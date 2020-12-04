ae84565e3fc5d4b2577c4623a30c9befe19aa31b
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-04 00:32:38 -0500

    implemented ISS Spotter II instructions to create fetchCoordsByIP (but had to change their URI)

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const {fetchMyIP} = require('./iss');
+const {fetchCoordsByIP} = require('./iss');
+
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -7,4 +9,14 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+});
+
+fetchCoordsByIP("142.119.98.170", (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coordinates);
+
 });
--- iss.js
+++ iss.js
@@ -17,4 +17,20 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) return callback(error, null);
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+});
+
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };

5059f3a8bc1f5ec1a03a76537a4fa20b0bed9d0e
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-03 23:25:41 -0500

    implemented per ISS Spotter I instructions

--- iss.js
+++ iss.js
@@ -2,15 +2,18 @@ const request = require('request');
 
 const fetchMyIP = function(callback) {
   request(`https://api.ipify.org?format=json`, (error, response, body) => {
-    if (error) {
-      callback(error, null);
-    } else {
-      if (response.statusCode !== 200) {
-        callback(error, null);
-      } else {
-        callback(null, JSON.parse(body).ip);
-      }
+    if (error) return callback(error, null);
+
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
     }
+    
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
+
   });
 };
 

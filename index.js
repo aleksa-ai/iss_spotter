1600c7a9158837b3d876dabd6008ac3d57e28653
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-06 23:53:46 -0500

    ready up to ISS Spotter IV

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const {fetchMyIP} = require('./iss');
-const {fetchCoordsByIP} = require('./iss');
+// const {fetchMyIP} = require('./iss');
+// const {fetchCoordsByIP} = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-fetchMyIP((error, ip) => {
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP("142.119.98.170", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned Coords:' , coordinates);
+
+// });
+
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
-
-fetchCoordsByIP("142.119.98.170", (error, coordinates) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  console.log('It worked! Returned Coords:' , coordinates);
-
+  console.log('It worked! Returned flyover times:' , passTimes);
 });

4b3b6daec48029a107018d3f448b19adc35c37f4
Aleksa Gavrilovic <lks_g@live.ca>
2020-12-07 01:32:03 -0500

    attempts at implementing  ISS Spotter With Promises

---
+++ iss_promised.js
@@ -0,0 +1,41 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = () =>  {
+  return request('https://api.ipify.org?format=json');
+};
+
+
+/*
+ * Makes a request to ipvigilante.com using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = (body) => {
+  const { latitude, longitude } = JSON.parse(body).data;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+// const nextISSTimesForMyLocation = () => {
+//   return fetchMyIP()
+//     .then(fetchCoordsByIP)
+//     .then(fetchISSFlyOverTimes)
+//     .then((data) => {
+//       const { response } = JSON.parse(data);
+//       return response;
+//     })
+//     .catch((error) => {
+//       console.log("It didn't work: ", error.message);
+//     });
+// };
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
